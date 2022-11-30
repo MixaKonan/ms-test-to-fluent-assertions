c8d229b1578d4899a54abbb975c214e450b29f6f
Mykhailo Konontsev <mykhailo.konontsev@perfectial.com>
2022-11-30 18:11:26 +0200

    Stable version

---
+++ MsTest.To.FluentAssertions/FileHelper.cs
@@ -0,0 +1,47 @@
+﻿namespace MsTest.To.FluentAssertions;
+
+public class FileHelper
+{
+    private const string NewFileSuffix = "_Fluent";
+    private const string CsFileExtension = ".cs";
+    private string Path { get; }
+    private bool PathIsDirectory { get; }
+    private string Directory { get; }
+
+    public FileHelper(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            throw new ArgumentException("Path cannot be empty", nameof(path));
+        }
+
+        if (!System.IO.Directory.Exists(path) && !File.Exists(path))
+        {
+            throw new ArgumentException($"No directory {path} found", nameof(path));
+        }
+
+        Path = path;
+        PathIsDirectory = System.IO.Path.GetExtension(path) == string.Empty;
+        Directory = System.IO.Path.GetDirectoryName(path)!;
+    }
+
+    public List<string> GetFilePaths()
+    {
+        var filePaths = new List<string>();
+        if (PathIsDirectory)
+        {
+            filePaths.AddRange(System.IO.Directory.GetFiles(Path).Where(fileName => fileName.EndsWith(CsFileExtension) && !fileName.Contains(NewFileSuffix)));
+        }
+        else
+        {
+            filePaths.Add(Path);
+        }
+
+        return filePaths;
+    }
+
+    public void SaveFile(string fileName, string fileContent)
+    {
+        File.WriteAllText(@$"{Directory}\{fileName}{NewFileSuffix}.cs", fileContent);
+    }
+}
---
+++ MsTest.To.FluentAssertions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using MsTest.To.FluentAssertions;
+using Spectre.Console;
+
+var path = args[0];
+
+var fileHelper = new FileHelper(path);
+var filePaths = fileHelper.GetFilePaths();
+
+var stringBuilder = new StringBuilder();
+var status = AnsiConsole.Status().Spinner(Spinner.Known.Pipe);
+
+status.Start("Processing files",
+    context =>
+    {
+        foreach (var filePath in filePaths)
+        {
+            var fileName = Path.GetFileName(filePath);
+            context.Status($"[red]Processing {fileName}[/]");
+
+            var fileText = File.ReadAllText(filePath);
+            var indexesOfAssertions = fileText.AllIndexesOf("Assert.");
+
+            for (var i = 0; i < fileText.Length;)
+            {
+                if (indexesOfAssertions.Contains(i))
+                {
+                    var assertion = new string(fileText[i..].TakeWhile(c => c != ';').ToArray());
+                    var fluentAssertion = FrameworksConverter.ToFluentAssertion(assertion);
+                    stringBuilder.Append(fluentAssertion);
+                    i += assertion.Length + 1 /* semicolon */;
+                    continue;
+                }
+
+                stringBuilder.Append(fileText[i]);
+                i++;
+            }
+
+            fileHelper.SaveFile(fileName, stringBuilder.ToString());
+            stringBuilder.Clear();
+        }
+
+        context.Status = "Finished processing files";
+    });
+
+AnsiConsole.Markup("[green]Finished processing files[/]");
