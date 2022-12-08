b600659059bb9ed23d584fe42128fe7d17f49cbe
Mykhailo Konontsev <fater181@gmail.com>
2022-12-08 02:11:35 +0200

    AstParser added

---
+++ MsTest.To.FluentAssertions/TestFile.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MsTest.To.FluentAssertions;
+
+[TestClass]
+public class TestFile
+{
+    [TestMethod]
+    public void AssertionTest()
+    {
+        var x = "jopa";
+        var q = "huy";
+        
+        Assert.AreEqual(q, "jopa");
+    }
+}
