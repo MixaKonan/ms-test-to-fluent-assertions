876719eec58ca90d3ede00155d8a83b41b6f9287
Mykhailo Konontsev <mykhailo.konontsev@perfectial.com>
2022-12-09 13:19:55 +0200

    Conversion approach changed to using abstract syntax tree

--- MsTest.To.FluentAssertions/Program.cs
+++ MsTest.To.FluentAssertions/Program.cs
@@ -1,43 +1,27 @@
-﻿using System.Text;
-using MsTest.To.FluentAssertions;
+﻿using MsTest.To.FluentAssertions.FileHelper;
+using MsTest.To.FluentAssertions.SyntaxParsing;
 using Spectre.Console;
 
 var path = args[0];
 
 var fileHelper = new FileHelper(path);
-var filePaths = fileHelper.GetFilePaths();
 
-var stringBuilder = new StringBuilder();
 var status = AnsiConsole.Status().Spinner(Spinner.Known.Pipe);
-
 status.Start("Processing files",
     context =>
     {
-        foreach (var filePath in filePaths)
+        foreach (var filePath in fileHelper.FilePaths)
         {
             var fileName = Path.GetFileName(filePath);
             context.Status($"[red]Processing {fileName}[/]");
 
             var fileText = File.ReadAllText(filePath);
-            var indexesOfAssertions = fileText.AllIndexesOf("Assert.");
+            var hadMsTestAssertions = CSharpMsTestToFluentConverter.TryConvert(fileText, out var newFileText);
 
-            for (var i = 0; i < fileText.Length;)
+            if (hadMsTestAssertions)
             {
-                if (indexesOfAssertions.Contains(i))
-                {
-                    var assertion = new string(fileText[i..].TakeWhile(c => c != ';').ToArray());
-                    var fluentAssertion = FrameworksConverter.ToFluentAssertion(assertion);
-                    stringBuilder.Append(fluentAssertion);
-                    i += assertion.Length + 1 /* semicolon */;
-                    continue;
-                }
-
-                stringBuilder.Append(fileText[i]);
-                i++;
+                fileHelper.SaveFile(fileName, newFileText);
             }
-
-            fileHelper.SaveFile(fileName, stringBuilder.ToString());
-            stringBuilder.Clear();
         }
 
         context.Status = "Finished processing files";
---
+++ MsTest.To.FluentAssertions/SyntaxParsing/FluentExpressionsExtensions.cs
@@ -0,0 +1,78 @@
+﻿namespace MsTest.To.FluentAssertions.SyntaxParsing;
+
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+public static class FluentExpressionsExtensions
+{
+    public static InvocationExpressionSyntax Should(this ArgumentSyntax invokeOn)
+    {
+        var shouldInvocation = SyntaxFactory.InvocationExpression(
+                SyntaxFactory.MemberAccessExpression(
+                    SyntaxKind.SimpleMemberAccessExpression,
+                    invokeOn.Expression,
+                    SyntaxFactory.IdentifierName(AssertionInfo.Should)));
+
+        return shouldInvocation;
+    }
+
+    public static InvocationExpressionSyntax Be(this InvocationExpressionSyntax should, ArgumentSyntax expected)
+    {
+        var fluentInvocation = should.Fluent(AssertionInfo.Be).WithArgument(expected).WithArgument(expected);
+
+        return fluentInvocation;
+    }
+
+    public static InvocationExpressionSyntax NotBe(this InvocationExpressionSyntax should, ArgumentSyntax expected)
+    {
+        var fluentInvocation = should.Fluent(AssertionInfo.NotBe).WithArgument(expected);
+
+        return fluentInvocation;
+    }
+
+    public static InvocationExpressionSyntax BeTrue(this InvocationExpressionSyntax should)
+    {
+        var fluentInvocation = should.Fluent(AssertionInfo.BeTrue);
+
+        return fluentInvocation;
+    }
+
+    public static InvocationExpressionSyntax BeFalse(this InvocationExpressionSyntax should)
+    {
+        var fluentInvocation = should.Fluent(AssertionInfo.BeFalse);
+
+        return fluentInvocation;
+    }
+
+    public static InvocationExpressionSyntax BeNull(this InvocationExpressionSyntax should)
+    {
+        var fluentInvocation = should.Fluent(AssertionInfo.BeNull);
+
+        return fluentInvocation;
+    }
+
+    public static InvocationExpressionSyntax NotBeNull(this InvocationExpressionSyntax should)
+    {
+        var fluentInvocation = should.Fluent(AssertionInfo.NotBeNull);
+
+        return fluentInvocation;
+    }
+
+    private static InvocationExpressionSyntax Fluent(this InvocationExpressionSyntax should, string methodName)
+    {
+        var fluentInvocation = SyntaxFactory.InvocationExpression(
+            SyntaxFactory.MemberAccessExpression(
+                SyntaxKind.SimpleMemberAccessExpression,
+                should,
+                SyntaxFactory.IdentifierName(methodName)));
+
+        return fluentInvocation;
+    }
+
+    private static InvocationExpressionSyntax WithArgument(this InvocationExpressionSyntax argumentExpectingAssertion, ArgumentSyntax argument)
+    {
+        return argumentExpectingAssertion.WithArgumentList(
+            SyntaxFactory.ArgumentList(
+                SyntaxFactory.SingletonSeparatedList(
+                    SyntaxFactory.Argument(argument.Expression))));
+    }
+}
